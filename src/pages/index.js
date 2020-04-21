26caaafd1af669f941afeab61650666937bd872f
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-21 17:08:17 -0500

    Estilizando componentes con styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 import { SEO, Jumbo } from '../components';
+import styled from 'styled-components';
 
 export const query = graphql`
   query GET_DESCRIPTION {
@@ -16,16 +17,22 @@ export const query = graphql`
   }
 `;
 
+const Button = styled.button`
+  width: 8rem;
+  color: ${(props) => props.color};
+  background-color: #98ca3f;
+  &:hover {
+    transform: scale(1.4);
+  }
+  border: none;
+  border-radius: 10px;
+`;
+
 const IndexPage = ({ data }) => (
   <>
     <SEO title='Home' />
     <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to='/thanks/'>Go to thanks</Link>
-    <br />
-    <Link to='/cancelled/'>Go to cancelled</Link>
+    <Button color='gray'>Comprar</Button>
   </>
 );
 
